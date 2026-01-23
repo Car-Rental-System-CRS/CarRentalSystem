8a1b86010fefcef1681c2be4ffce310ca4ee4239
nghiapro0982 <haquangnghia123@gmail.com>
2026-01-23 19:25:06 +0700

    add dependency, deleted dont need entity

--- src/main/java/src/Entity/repo/VehicleRepository.java
+++
@@ -1,8 +0,0 @@
-
-package src.Entity.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import src.entity.Vehicle;
-
-public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
-}
